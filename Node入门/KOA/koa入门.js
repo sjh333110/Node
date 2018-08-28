23214107d1eac5871cc0a53a6578ed10881f0f7b
song_my <song_my@example.con>
2018-08-28 10:10:12 +0800

    ddd

--- Node入门/KOA/koa入门.js
+++ Node入门/KOA/koa入门.js
@@ -1,11 +1,10 @@
-var express = require('express');
+var koa = require('koa');
+var app = koa();
 
-var app = express();
-
-app.get('/', function (req,res){
-    res.send("Hello World");
+app.use('/test', function *() {
+    yield doReadFile1();
+    var data = yield doReadFile12();
+    this.body = data;
 });
 
-app.listen(3000, function() {
-    console.log("Example app listening on port 3000");
-})
+app.listen(3000);
